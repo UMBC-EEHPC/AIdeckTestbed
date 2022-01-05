18659345499ba25eaf889ff45b7024b57a2c054d
Edward Humes <ehumes2@umbc.edu>
2022-01-05 18:31:22 -0500

    [Meta] Added support for building exported neural networks to run on the AI deck

---
+++ Model/ptq_int8.h
@@ -0,0 +1,26 @@
+
+#ifndef __MNIST_H__
+#define __MNIST_H__
+
+#define __PREFIX(x) ptq_int8 ## x
+
+#include "Gap.h"
+
+#ifdef __EMUL__
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/param.h>
+#include <string.h>
+extern AT_HYPERFLASH_FS_EXT_ADDR_TYPE ptq_int8_L3_Flash;
+#endif
+
+#if defined(USE_HYPER)
+extern AT_HYPERFLASH_FS_EXT_ADDR_TYPE ptq_int8_L3_Flash;
+#elif defined(USE_SPI)
+extern AT_QSPIFLASH_FS_EXT_ADDR_TYPE ptq_int8_L3_Flash;
+#endif
+
+
+#endif
