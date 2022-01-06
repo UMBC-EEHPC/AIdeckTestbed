98ffd29710b14156224c12c5de46fe67966afec3
Edward Humes <ehumes2@umbc.edu>
2022-01-06 15:09:51 -0500

    [Meta] Updated launcher scripts to handle uploading models to GVSOC/AIdeck

--- Scripts/run_in_board.sh
+++ Scripts/run_in_board.sh
@@ -1,6 +1,13 @@
-gapy --target=ai_deck --platform=board --work-dir="$1" --config-ini="$1"/../uart.ini      run --image --binary="$1"/"$2"
-gapy --target=ai_deck --platform=board --work-dir="$1" --config-ini="$1"/../uart.ini      run --flash --binary="$1"/"$2"
-gapy --target=ai_deck  gen_flash_image --boot-loader "$1"/"$2"  -o "$1"/flash.img 
-gapy --target=ai_deck --platform=board --work-dir="$1" --config-ini="$1"/../uart.ini      run --exec-prepare --exec --binary="$1"/"$2"
+#!/bin/bash
 
-gap8-openocd -d0 -c 'gdb_port disabled; telnet_port disabled; tcl_port disabled' -f "$GAPY_OPENOCD_CABLE" -f target/gap8revb.tcl -f tcl/jtag_boot_entry.tcl -c 'gap8_jtag_load_binary_and_start "$1"/"$2" elf 0x1c000080'
+gapy --target=ai_deck --platform=board --work-dir="$1" --config-ini="$1"/../uart.ini --config-opt=**/flash/content/partitions/readfs/files="$1"/ptq_int8_L3_Flash_Const.dat --config-opt=flash/content/partitions/readfs/files="$1"/ptq_int8_L3_Flash_Const.dat run --image --binary="$1"/"$2"
+gapy --target=ai_deck --platform=board --work-dir="$1" --config-ini="$1"/../uart.ini --config-opt=**/flash/content/partitions/readfs/files="$1"/ptq_int8_L3_Flash_Const.dat --config-opt=flash/content/partitions/readfs/files="$1"/ptq_int8_L3_Flash_Const.dat run --flash --binary="$1"/"$2"
+
+FLASH_IMG_SIZE=$(wc -c < "$1"/target.board.devices.flash.img)
+gap8-openocd -d0 -c "gdb_port disabled; telnet_port disabled; tcl_port disabled" -c "script interface/ftdi/olimex-arm-usb-tiny-h.cfg; script target/gap8revb.tcl; script tcl/flash_image.tcl; script tcl/jtag_boot.tcl; gap_flash_raw_hyper ${1}/target.board.devices.flash.img ${FLASH_IMG_SIZE} ${1}/../Toolchain/gap_sdk/utils/openocd_tools; exit;"
+
+gapy --target=ai_deck  gen_flash_image --boot-loader "$1"/"$2"  -o "$1"/flash.img 
+
+gapy --target=ai_deck --platform=board --work-dir="$1" --config-ini="$1"/../uart.ini --config-opt=**/flash/content/partitions/readfs/files="$1"/ptq_int8_L3_Flash_Const.dat --config-opt=flash/content/partitions/readfs/files="$1"/ptq_int8_L3_Flash_Const.dat run --exec-prepare --exec --binary="$1"/"$2"
+
+gap8-openocd -d0 -c 'gdb_port disabled; telnet_port disabled; tcl_port disabled' -f "$GAPY_OPENOCD_CABLE" -f target/gap8revb.tcl -f tcl/jtag_boot_entry.tcl -c 'gap8_jtag_load_binary_and_start '"$1"'/'"$2"' elf 0x1c000080'
