cb0894bdfa28e446f3eacb15314ee6cfae5c700a
Edward Humes <ehumes2@umbc.edu>
2022-01-09 22:37:11 -0500

    [Core/Support] Fixed assert_gap8 since beforehand, the argument wasn't escaped and it could potentially cause incorrect assertions

--- Core/Support/Assertions.h
+++ Core/Support/Assertions.h
@@ -3,7 +3,7 @@
 #include <Core/Support/gap_sdk.h>
 
 #define assert_gap8(x) \
-    if (!x) { \
+    if (!(x)) { \
         printf("ASSERTION FAILED %s:%d %s on core %ld\n", __FILE__, __LINE__, __PRETTY_FUNCTION__, pi_core_id()); \
         pmsis_exit(-1); \
     }
