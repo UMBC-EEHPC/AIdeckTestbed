996baf519fb2a3f1ff58038fd3fe407d2c7671df
aurxenon <ehumes2@umbc.edu>
2022-03-29 23:01:53 -0400

    [Toolchain] Updated to the latest version of the GAP SDK

--- etl_profile.h
+++ etl_profile.h
@@ -1,7 +1,10 @@
 #ifndef ETL_PROFILE_H
 #define ETL_PROFILE_H
 
-#include <etl/profiles/cpp17_no_stl.h>
-#include <etl/profiles/gcc_generic_no_stl.h>
+/*#include <etl/profiles/cpp17_no_stl.h>
+#include <etl/profiles/gcc_generic_no_stl.h>*/
+
+#define ETL_NO_STL 1
+#define ETL_CPP17_SUPPORTED 1
 
 #endif
